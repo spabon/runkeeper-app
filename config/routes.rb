914f10ab6ce206cc8b2bf66ea42d04139cb0fe3a
Gustavo Jose Morales Carpio <gustavo.morales@gmail.com>
2015-05-09 00:45:39 +0000

    Add Devise authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :walks
 
   resources :users
@@ -8,7 +9,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root 'users#index'
+  root 'walks#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
